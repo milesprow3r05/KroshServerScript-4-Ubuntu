53c9c2cc1c205c050cba3e9c6b13fabc4051db68
Андрей <azazaandrey407@gmail.com>
2021-04-15 18:21:59 +0300

    Added PHP/Apache modules, major fixes

--- kroshserver.sh
+++ kroshserver.sh
@@ -5,14 +5,24 @@ then
   exit
 fi
 echo "Apache2, PHP5.6, and MySQL, for manage - Adminer for MySQL"
-apt install -y apache2 python-pip software-properties-common nodejs
+apt install -y apache2 mysql-server neofetch screenfetch python-pip software-properties-common nodejs
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt update
-sudo apt install php5.6 php5.6-xml php5.6-gd php5.6-mysql php5.6-sqlite3
+sudo apt install -y php5.6 php5.6-xml php5.6-gd php5.6-mysql php5.6-sqlite3 php5.6-curl php5.6-opcache php5.6-json php5.6-xmlrpc php5.6-xmlrpc
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt update
+sudo apt install certbot
+sudo certbot run --apache
 cd $1
 wget -P $1 https://github.com/vrana/adminer/releases/download/v4.8.0/adminer-4.8.0-mysql.php
 wget -P $1 https://github.com/kroshmorkovkin/KroshServerScript-4-Ubuntu/raw/main/adminer.css
+wget https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_amd64.deb
+sudo dpkg -i mod-pagespeed-*.deb
+sudo apt -f install
+sudo systemctl restart apache2
 mv -f adminer-4.8.0-mysql.php adminer.phtml
+a2enmod status
+a2enmod info
 a2enmod cgi
 a2enmod mime_magic
 a2enmod alias
