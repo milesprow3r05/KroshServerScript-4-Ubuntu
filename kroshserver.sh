a21b8b3c390537312c36c5633cb5a20467dd9dcf
Larry Holst <kotman2000@yandex.ru>
2021-04-15 10:50:50 +0300

    Update kroshserver.sh

--- kroshserver.sh
+++ kroshserver.sh
@@ -10,7 +10,7 @@ sudo add-apt-repository -y ppa:ondrej/php
 sudo apt update
 sudo apt install php5.6 php5.6-xml php5.6-gd php5.6-mysql php5.6-sqlite3
 cd $1
-wget -P $1 https://github.com/kroshmorkovkin/KroshServerScript-4-Ubuntu/raw/main/adminer-4.8.0-mysql.php
+wget -P $1 https://github.com/vrana/adminer/releases/download/v4.8.0/adminer-4.8.0-mysql.php
 wget -P $1 https://github.com/kroshmorkovkin/KroshServerScript-4-Ubuntu/raw/main/adminer.css
 mv -f adminer-4.8.0-mysql.php adminer.phtml
 a2enmod cgi
