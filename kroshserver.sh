3769be6de340c94c9aa9e83d81edfd23d9dccf44
Andrey <azazaandrey407@gmail.com>
2021-06-02 16:07:07 +0300

    Changed Adminer link
    (now always download the latest versions)

--- kroshserver.sh
+++ kroshserver.sh
@@ -5,7 +5,9 @@ then
   exit
 fi
 echo "Apache2, PHP5.6, and MySQL, for manage - Adminer for MySQL"
-apt install -y apache2 mysql-server neofetch screenfetch python-pip software-properties-common nodejs
+echo "by kroshmorkovkin"
+sudo apt install -y apache2 mysql-server neofetch screenfetch python-pip software-properties-common nodejs
+sudo mysql_secure_installation
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt update
 sudo apt install -y php5.6 php5.6-xml php5.6-gd php5.6-mysql php5.6-sqlite3 php5.6-curl php5.6-opcache php5.6-json php5.6-xmlrpc
@@ -14,18 +16,19 @@ sudo apt update
 sudo apt install certbot
 sudo certbot run --apache
 cd $1
-wget -P $1 https://github.com/vrana/adminer/releases/download/v4.8.0/adminer-4.8.0-mysql.php
+wget -P $1  https://www.adminer.org/latest-mysql.php
 wget -P $1 https://github.com/kroshmorkovkin/KroshServerScript-4-Ubuntu/raw/main/adminer.css
 wget https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_amd64.deb
 sudo dpkg -i mod-pagespeed-*.deb
 sudo apt -f install
-sudo systemctl restart apache2
+sudo rm mod-pagespeed-*.deb
 mv -f adminer-4.8.0-mysql.php adminer.phtml
-a2enmod status
-a2enmod info
-a2enmod cgi
-a2enmod mime_magic
-a2enmod alias
-a2enmod rewrite
-a2enmod lua
+sudo a2enmod status
+sudo a2enmod info
+sudo a2enmod cgi
+sudo a2enmod mime_magic
+sudo a2enmod alias
+sudo a2enmod rewrite
+sudo a2enmod lua
+sudo systemctl restart apache2
 echo "Done"
